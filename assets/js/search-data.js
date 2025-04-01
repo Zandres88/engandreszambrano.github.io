08b857d9f1e0758c0569604c64a9f9771d98a1bc
Zandres88 <41597664+Zandres88@users.noreply.github.com>
2025-04-01 20:55:02 +0000

    Deploying to gh-pages from @ Zandres88/engandreszambrano.github.io@cc2c0cdfdd77ef4629c6029e69e4989fbf2bfb51 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/engandreszambrano.github.io/projects/";
           },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/engandreszambrano.github.io/repositories/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "",
@@ -135,35 +142,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/engandreszambrano.github.io//feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("mailto:%7A%61%6E%64%72%65%73_%38%38@%68%6F%74%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
       id: 'light-theme',
