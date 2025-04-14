60602169279564d319bfb9ccdd9862be6ab5ab36
Zandres88 <41597664+Zandres88@users.noreply.github.com>
2025-04-14 21:43:42 +0000

    Deploying to gh-pages from @ Zandres88/engandreszambrano.github.io@2aff9163fb66d9c6b1d4062269807e0b40dd20f6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -50,9 +50,9 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/engandreszambrano.github.io/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-information-security-standards-and-frameworks",
+          title: 'Information Security Standards and Frameworks',
+          description: "This project is based on the research and presentation of the most relevant information security frameworks and policies applied within a business environment. It highlights essential standards such as NIST, ISO/IEC, and COBIT, offering a structured overview of their roles in managing cyber risks and strengthening organizational security posture.",
           section: "Projects",handler: () => {
               window.location.href = "/engandreszambrano.github.io/projects/1_project/";
             },},{id: "projects-project-2",
