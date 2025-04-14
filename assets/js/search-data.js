4dcd2c01ac76c6ec3272567415d33256d194ebb0
Zandres88 <41597664+Zandres88@users.noreply.github.com>
2025-04-14 22:41:14 +0000

    Deploying to gh-pages from @ Zandres88/engandreszambrano.github.io@6935de9fd57a4b033e2c7e7585710735ccb3965c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,9 +55,9 @@ ninja.data = [{
           description: "This project is based on the research and presentation of the most relevant information security frameworks and policies applied within a business environment. It highlights essential standards such as NIST, ISO/IEC, and COBIT, offering a structured overview of their roles in managing cyber risks and strengthening organizational security posture.",
           section: "Projects",handler: () => {
               window.location.href = "/engandreszambrano.github.io/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/engandreszambrano.github.io/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
